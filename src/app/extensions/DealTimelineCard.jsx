0b01f12fc2620c23757eda6d427902286caa4585
revmachinery <stephanie@revelationmachinery.com>
2025-08-28 12:31:03 -0400

    Update and rename ListingsCard.jsx to DealTimelineCard.jsx

--- src/app/extensions/DealTimelineCard.jsx
+++ src/app/extensions/DealTimelineCard.jsx
@@ -3,8 +3,9 @@ import { hubspot } from "@hubspot/ui-extensions";
 hubspot.extend(({ context }) => {
   return (
     <div style={{ padding: "16px" }}>
-      <h3>Hello from Listings!</h3>
-      <p>Record ID: {context.objectId}</p>
+      <h3>Deal Timeline</h3>
+      <p>Deal ID: {context.objectId}</p>
+      <p>This is where your custom timeline view will go.</p>
     </div>
   );
 });
